6f17eb2681788d7a5114da5a291c775169f7db81
pipedev <felipe.nava.co@gmail.com>
2019-06-04 20:08:38 -0700

    separate boards for each user

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,22 @@
-import React, { Fragment } from 'react';
-import ColumnsContainer from './cols/ColumnsContainer';
-import Header from './Header';
+import React from 'react';
 import Page from './Page';
-import ListControls from './lists/ListControls';
-import base, { firebaseApp } from '../base';
+import Board from './Board';
+import Header from './Header';
+import { firebaseApp } from '../base';
 
 class App extends React.Component {
     constructor (props) {
         super(props);
+
         this.state = {
-            board: {},
-            user: null,
-            board: {}
+            user: null
         }
     }
 
     authListener () {
         firebaseApp.auth().onAuthStateChanged((user) => {
             if (user) {
-                this.setState({
-                    user: user
-                });
-
-                this.ref = base.syncState(this.state.user.uid, {
-                    context: this,
-                    state: 'board'
-                });
+                this.setState({ user });
             } else {
                 this.setState({
                     user: null
@@ -34,132 +25,16 @@ class App extends React.Component {
         });
     }
 
-    addList = listTitle => {
-        let state = { ...this.state.firebaseLists };
-
-        if (!listTitle) {
-            return;
-        }
-
-        if (Object.keys(state).length === 0) {
-            state.counter = 1;
-        } else {
-            state.counter = state.counter + 1;
-        }
-
-        if (!state.lists) {
-            state.lists = {};
-        }
-
-        state.lists[state.counter] = {
-            title: listTitle,
-            itemsObj: {
-                counter: 0
-            }
-        };
-
-        this.setState({ firebaseLists: state });
-    }
-
-    updateTitle = (key, value) => {
-        let firebaseLists = { ...this.state.firebaseLists };
-        firebaseLists.lists[key].title = value.target.value;
-        this.setState({ firebaseLists });
-    }
-
-    removeList = key => {
-        let lists = { ...this.state.firebaseLists.lists };
-
-        lists[key] = null;
-
-        this.setState({
-            firebaseLists: {
-                lists: lists
-            }
-        });
-    }
-
-    addItem = (itemTitle, listKey) => {
-        let lists = this.state.firebaseLists.lists;
-        let list = lists[listKey];
-        let itemsObj = { ...list.itemsObj };
-
-        if (!itemTitle) {
-            console.error('Can not create an item without title');
-            return;
-        }
-
-        if (!list) {
-            console.error('List key is not recognized');
-            return;
-        }
-
-        if (itemsObj.counter === 0) {
-            itemsObj.items = {};
-        }
-
-        itemsObj.counter = itemsObj.counter + 1;
-
-        itemsObj.items[itemsObj.counter] = {
-            title: itemTitle,
-            status: 1,
-            descript: '',
-            assignedPeople: {},
-            owner: 1,
-            labels: {}
-        };
-
-        list.itemsObj = itemsObj;
-        lists[listKey] = list;
-
-        this.setState({
-            firebaseLists: {
-                lists: lists
-            }
-        });
-    }
-
     componentDidMount() {
         this.authListener();
     }
 
-    updateItem = (item, itemKey, listKey) => {
-        let lists = this.state.firebaseLists.lists;
-        let list = lists[listKey];
-        let itemsObj = { ...list.itemsObj };
-
-        itemsObj.items[itemKey] = item;
-
-        list.itemsObj = itemsObj;
-        lists[listKey] = list;
-
-        this.setState({
-            firebaseLists: {
-                lists: lists
-            }
-        });
-    }
 
     render() {
         return (
             <div id='content'>
             <Header />
-                { this.state.user ? (
-                    <Fragment>
-                        <ListControls addList={this.addList} />
-                        <div id='board-container'>
-                            <div id='board' className='colsView'>
-                                <ColumnsContainer
-                                    lists={this.state.firebaseLists.lists}
-                                    removeList={this.removeList}
-                                    updateTitle={this.updateTitle}
-                                    addItem={this.addItem}
-                                    updateItem={this.updateItem} />
-                            </div>
-                        </div>
-                    </Fragment>
-                    ) :  (<Page />)
-                }
+                { this.state.user ? (<Board uid={this.state.user.uid}/>) :  (<Page />) }
             </div>
         )
     }
---
+++ src/components/Board.js
@@ -0,0 +1,143 @@
+import React, { Fragment } from 'react';
+import ColumnsContainer from './cols/ColumnsContainer';
+import ListControls from './lists/ListControls';
+import base from '../base';
+
+class Board extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            board: {}
+        }
+    }
+
+    addList = listTitle => {
+        let state = { ...this.state.board };
+
+        if (!listTitle) {
+            return;
+        }
+
+        if (Object.keys(state).length === 0) {
+            state.counter = 1;
+        } else {
+            state.counter = state.counter + 1;
+        }
+
+        if (!state.lists) {
+            state.lists = {};
+        }
+
+        state.lists[state.counter] = {
+            title: listTitle,
+            itemsObj: {
+                counter: 0
+            }
+        };
+
+        this.setState({ board: state });
+    }
+
+    updateTitle = (key, value) => {
+        let firebaseLists = { ...this.state.firebaseLists };
+        firebaseLists.lists[key].title = value.target.value;
+        this.setState({ firebaseLists });
+    }
+
+    removeList = key => {
+        let lists = { ...this.state.board.lists };
+
+        lists[key] = null;
+
+        this.setState({
+            board: {
+                lists: lists
+            }
+        });
+    }
+
+    addItem = (itemTitle, listKey) => {
+        let lists = this.state.board.lists;
+        let list = lists[listKey];
+        let itemsObj = { ...list.itemsObj };
+
+        if (!itemTitle) {
+            console.error('Can not create an item without title');
+            return;
+        }
+
+        if (!list) {
+            console.error('List key is not recognized');
+            return;
+        }
+
+        if (itemsObj.counter === 0) {
+            itemsObj.items = {};
+        }
+
+        itemsObj.counter = itemsObj.counter + 1;
+
+        itemsObj.items[itemsObj.counter] = {
+            title: itemTitle,
+            status: 1,
+            descript: '',
+            assignedPeople: {},
+            owner: 1,
+            labels: {}
+        };
+
+        list.itemsObj = itemsObj;
+        lists[listKey] = list;
+
+        this.setState({
+            board: {
+                lists: lists
+            }
+        });
+    }
+
+    updateItem = (item, itemKey, listKey) => {
+        let lists = this.state.board.lists;
+        let list = lists[listKey];
+        let itemsObj = { ...list.itemsObj };
+
+        itemsObj.items[itemKey] = item;
+
+        list.itemsObj = itemsObj;
+        lists[listKey] = list;
+
+        this.setState({
+            board: {
+                lists: lists
+            }
+        });
+    }
+
+    componentDidMount () {
+        this.ref = base.syncState(`board-${this.props.uid}`, {
+            context: this,
+            state: 'board'
+        });
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <ListControls addList={this.addList} />
+                <div id='board-container'>
+                    <div id='board' className='colsView'>
+                        <ColumnsContainer
+                            lists={this.state.board.lists}
+                            removeList={this.removeList}
+                            updateTitle={this.updateTitle}
+                            addItem={this.addItem}
+                            updateItem={this.updateItem} />
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Board;
